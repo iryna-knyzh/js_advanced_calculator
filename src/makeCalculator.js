002102aa12fc2e48bbcfdc8e401d6dec68e404d7
Iryna Knyzh <iryna.knyzh@gmail.com>
2025-10-07 13:42:31 +0300

    task

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,33 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+    add: function (a) {
+      calculator.result += a;
+    },
+    subtract: function (a) {
+      calculator.result -= a;
+    },
+    multiply: function (a) {
+      calculator.result *= a;
+    },
+    divide: function (a) {
+      calculator.result /= a;
+    },
+    reset: function () {
+      calculator.result = 0;
+
+      return this;
+    },
+    operate: function (callback, a) {
+      callback(a);
+
+      return this;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
